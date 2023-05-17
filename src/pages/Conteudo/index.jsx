de297a9106054e414346747cb8d3d5a80e31193e
Adriano <adriano_alves_2008@hotmail.com>
2023-05-16 21:18:45 -0300

    estruturando a home

--- src/pages/Conteudo/index.jsx
+++ src/pages/Conteudo/index.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import { useState } from 'react'
+
+import * as C from './style'
 
 function Conteudo() {
-  return <div>Conteudo</div>
+  const [query, setQuery] = useState('')
+  const [posts, setPosts] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+  return (
+    <C.Container>
+      <C.Header>
+        <h1>Veja os nossos posts mais recentes!</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Ou busque por tags..."
+            onChange={e => setQuery(e.target.value)}
+          />
+          <button type="submit">Pesquisar</button>
+        </form>
+      </C.Header>
+
+      <C.Content>
+        {posts && posts.length === 0 && (
+          <div className="noPosts">
+            <h3>Não foram encontrados posts</h3>
+          </div>
+        )}
+      </C.Content>
+    </C.Container>
+  )
 }
 
 export default Conteudo
